7ebe189787576666c719f45a09a868e036118972
Patrick Pedersen <ctx.xda@gmail.com>
2022-04-09 19:37:44 +0200

    Config: Replaced pot, inversion no longer required
    Signed-off-by: Patrick Pedersen <ctx.xda@gmail.com>

--- src/config.h
+++ src/config.h
@@ -56,7 +56,7 @@
 
 #define BRIGHTNESS_POT_ADMUX_MSK (1 << MUX1)                  // PB4 (Refer to table 17-4 in the ATtiny25/45/85 datasheet)
 
-#define INVERT_POT                                         // Uncomment if pot is inverted
+// #define INVERT_POT                                         // Uncomment if pot is inverted
 
 // #define ADC_AVG_SAMPLES XX                                 // Max 255 - Number of samples used to determine the average potentiometer value.
                                                               // Increase this if the LED strip is noisy, especially at lower settings.
